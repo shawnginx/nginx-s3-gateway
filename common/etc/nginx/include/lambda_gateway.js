ddcd16a0f6d7e4b70c801d0ef69c5fa23a68ac55
Shawn Kim <shawnhan.kim@gmail.com>
2023-03-01 03:21:22 -0800

    fix: read credentials with session token

--- common/etc/nginx/include/lambda_gateway.js
+++ common/etc/nginx/include/lambda_gateway.js
@@ -236,10 +236,12 @@ function _writeCredentialsToFile(credentials) {
  */
 function readCredentials(r) {
     if (process.env['S3_ACCESS_KEY_ID'] && process.env['S3_SECRET_KEY']) {
+        const sessionToken = 'S3_SESSION_TOKEN' in process.env ?
+            process.env['S3_SESSION_TOKEN'] : null;
         return {
             accessKeyId: process.env['S3_ACCESS_KEY_ID'],
             secretAccessKey: process.env['S3_SECRET_KEY'],
-            sessionToken: null,
+            sessionToken: sessionToken,
             expiration: null
         };
     }
